3ac92591dd3f739c795f4fd7fd78155898100b4f
蠢羊 <1141079850@qq.com>
2022-08-25 16:30:58 +0800

    chore: demo文件夹初始化

---
+++ packages/demo/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">demo
+    <Counter />
+  </view>
+</template>
+
+<script>
+import './index.less'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Counter
+  }
+}
+</script>
